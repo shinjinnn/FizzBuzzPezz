a3145c07f20e244e27236588bf0fbb14a38e899c
shinjinnn <52521816+shinjinnn@users.noreply.github.com>
2020-02-28 17:27:16 +0900

    三項演算子

--- FizzBuzzPezz.js
+++ FizzBuzzPezz.js
@@ -22,14 +22,14 @@ for (let i = 1; i <= 120; i++) {
   const hoge = document.getElementById("FizzBuzzPezz");
   const para = document.createElement("p");
 
-  result = i % 30 === 0 && "FizzBuzzPezz " || 
-                i % 6 === 0 && "FizzBuzz " ||
-                i % 10 === 0 && "FizzPezz " ||
-                i % 15 === 0 && "BuzzPezz " || 
-                i % 2 === 0 && "Fizz " ||
-                i % 3 === 0 && "Buzz " ||
-                i % 5 === 0 && "Pezz " ||
-                i+ " ";
+  result = i % 30 === 0 ? "FizzBuzzPezz " 
+         : i % 6 === 0 ? "FizzBuzz " 
+         : i % 10 === 0 ? "FizzPezz " 
+         : i % 15 === 0 ? "BuzzPezz " 
+         : i % 2 === 0 ? "Fizz " 
+         : i % 3 === 0 ? "Buzz " 
+         : i % 5 === 0 ? "Pezz " 
+         : i+ " ";
   para.innerText = i + " " + result;
   hoge.appendChild(para);
 }
